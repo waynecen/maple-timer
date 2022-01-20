f39ddcf8b579592ed20b4f99dd9ba8cf804b3c9f
Wayne <wayne.cen@gmail.com>
2022-01-20 13:21:19 -0500

    initial commit, styling html and CSS

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// timer logic
+
+/* 
+run timer constantly ticks down when start run is pressed
+    'next fma in' also starts ticking down at the same time
+if p1 is selected, when next-fma button clicked,
+reduce fake timer by 150s
+    'next fma in' is reduced by 15 
+    
+if p2, reduce fake timer by 120s
+if p3, reduce fake timer by 100s*/
